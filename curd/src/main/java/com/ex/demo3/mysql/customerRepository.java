4a01fe504f493ab836b71a46796ec22acfd1d7b0
bhadu05 <85948798+bhadu05@users.noreply.github.com>
2021-06-22 09:49:02 +0530

    Update customerRepository.java

--- curd/src/main/java/com/ex/demo3/mysql/customerRepository.java
+++ curd/src/main/java/com/ex/demo3/mysql/customerRepository.java
@@ -2,6 +2,18 @@ package com.ex.demo3.mysql;
 import org.springframework.data.repository.CrudRepository;
 
 public interface customerRepository extends CrudRepository<customer, Integer> {
+  
+       public customer findByName(String Name);
+     public customer findByAge(int age);
+     @Query(
+             value = "SELECT name FROM customer",
+             nativeQuery = true)
+     List<String> CustomQuery();
+
+     @Modifying
+     @Transactional
+     @Query(value="UPDATE customer c SET c.age=56 WHERE c.age=24",nativeQuery = true)
+     void SetAge();
 
 
 }
